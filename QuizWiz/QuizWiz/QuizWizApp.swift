279ec3d5d90812ee09e24179e3b677ba881b2805
valentina.jelavic <valentina.jelavic@gmail.com>
2024-01-23 15:05:16 +0100

    Create my  application

---
+++ QuizWiz/QuizWiz/QuizWizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizWizApp.swift
+//  QuizWiz
+//
+//  Created by student on 23.01.2024..
+//
+
+import SwiftUI
+
+@main
+struct QuizWizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
