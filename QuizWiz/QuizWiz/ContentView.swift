7ae282ab5c558d7c423c03551019fab7151e8aba
valentina.jelavic <valentina.jelavic@gmail.com>
2024-01-26 17:58:44 +0100

    Add changes to main view

--- QuizWiz/QuizWiz/ContentView.swift
+++ QuizWiz/QuizWiz/ContentView.swift
@@ -7,13 +7,49 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text("QuizWiz")
+                .padding()
+                .foregroundColor(Color.purple)
+                .cornerRadius(10)
+                .font(.system(size: 65, weight: .bold))
+                
+            
+            
+            Image("wizardFinal")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 300)
+                .clipShape(RoundedRectangle(cornerRadius: 65))
+            
+            VStack {
+                Text("GETS STARTED")
+                    .padding()
+                    .background(Color.purple)
+                    .font(.system(size: 20, weight: .bold))
+                    .foregroundColor(.white)
+                    .cornerRadius(80)
+                
+                
+                
+                Text ("I already have an account")
+                    .padding()
+                    .foregroundColor(Color.purple)
+                    .font(.system(size: 15, weight: .bold))
+                    .background(
+                        RoundedRectangle(cornerRadius: 80)
+                            .stroke(Color.purple, lineWidth: 2)
+                            .background(Color.white)
+                    )
+                
+                
+                
+            }
         }
         .padding()
     }
